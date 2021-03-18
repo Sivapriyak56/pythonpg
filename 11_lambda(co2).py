62d7c7fe21d1c000a70d23ef30fa09bd5319405a
Sivapriyak56 <sivapriyakalari@gmail.com>
2021-03-18 11:03:48 -0400

    c0msg3

---
+++ 11_lambda(co2).py
@@ -0,0 +1,12 @@
+print("area of square:")
+x=lambda a:a*a
+print(x(4))
+
+print("area of rectangle")
+y=lambda w,l:w*l
+print(y(4,6))
+
+print("area of triangle")
+g=lambda w,l:w*l/2
+print(g(3,6))
+
